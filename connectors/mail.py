273e12ca9dee6d99f4f8309ffba87ac57a311421
Halit Dincer <github@halitdincer.com>
2023-12-20 00:45:40 -0500

    fetch emaild and return as string instead of csv file

--- connectors/mail.py
+++ connectors/mail.py
@@ -5,8 +5,9 @@ import json
 from datetime import datetime
 
 
+def fetch_emails_from_today(username, password, server, port=993):
+    email_data = "Sender,Subject,Date Sent,Email Content\n"  # CSV header
 
-def fetch_emails_from_today_csv(username, password, server, port=993, file_path='emails_today.csv'):
     try:
         # Connect to the IMAP server
         mail = imaplib.IMAP4_SSL(server, port)
@@ -23,55 +24,49 @@ def fetch_emails_from_today_csv(username, password, server, port=993, file_path=
         if result != 'OK':
             return "No messages found for today!"
 
-        # Open the file to write emails
-        with open(file_path, 'w', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
-            # Writing the headers
-            writer.writerow(["Sender", "Subject", "Date Sent", "Email Content"])
+        for num in data[0].split():
+            result, data = mail.fetch(num, "(RFC822)")
+            if result != 'OK':
+                continue
 
-            for num in data[0].split():
-                result, data = mail.fetch(num, "(RFC822)")
-                if result != 'OK':
+            # Parse the email content
+            msg = email.message_from_bytes(data[0][1])
+
+            # Extracting details from the header
+            from_ = msg.get("From").replace(',', ';')  # Replace commas to preserve CSV format
+            subject = msg.get("Subject").replace(',', ';')
+            date_sent = msg.get("Date").replace(',', ';')
+            content = ""
+
+            for part in msg.walk():
+                # If part is multipart, continue
+                if part.is_multipart():
                     continue
 
-                # Parse the email content
-                msg = email.message_from_bytes(data[0][1])
+                # Get the content type
+                content_type = part.get_content_type()
+                content_disposition = str(part.get("Content-Disposition"))
 
-                # Extracting details from the header
-                from_ = msg.get("From")
-                subject = msg.get("Subject")
-                date_sent = msg.get("Date")
-                content = ""
+                # If part is text/plain or text/html
+                if content_type in ("text/plain", "text/html") and "attachment" not in content_disposition:
+                    charset = part.get_content_charset()
+                    payload = part.get_payload(decode=True)
+                    
+                    # Decode payload with detected charset or fall back to ISO-8859-1
+                    try:
+                        if charset is not None:
+                            content += payload.decode(charset)
+                        else:
+                            content += payload.decode('ISO-8859-1')
+                    except UnicodeDecodeError:
+                        content += payload.decode('ISO-8859-1', errors='replace')
 
-                for part in msg.walk():
-                    # If part is multipart, continue
-                    if part.is_multipart():
-                        continue
-
-                    # Get the content type
-                    content_type = part.get_content_type()
-                    content_disposition = str(part.get("Content-Disposition"))
-
-                    # If part is text/plain or text/html
-                    if content_type in ("text/plain", "text/html") and "attachment" not in content_disposition:
-                        charset = part.get_content_charset()
-                        payload = part.get_payload(decode=True)
-                        
-                        # Decode payload with detected charset or fall back to ISO-8859-1
-                        try:
-                            if charset is not None:
-                                content += payload.decode(charset)
-                            else:
-                                content += payload.decode('ISO-8859-1')
-                        except UnicodeDecodeError:
-                            content += payload.decode('ISO-8859-1', errors='replace')
-
-                # Write the email details as a row in the CSV file
-                writer.writerow([from_, subject, date_sent, content])
+            # Concatenate the email details into the string
+            email_data += f'"{from_}","{subject}","{date_sent}","{content}"\n'
 
         mail.close()
         mail.logout()
-        return "Emails from today have been written to " + file_path
+        return email_data
 
     except Exception as e:
         return str(e)
@@ -80,7 +75,6 @@ def fetch_emails_from_today_csv(username, password, server, port=993, file_path=
 with open('../config.json', 'r') as file:
     config = json.load(file)
 
-# Usage example (replace with your details and desired file path)
-result = fetch_emails_from_today_csv(config["MAIL_ADDRESS"], config["MAIL_PASSWORD"], config["MAIL_SERVER"], file_path='emails_today.txt')
-print(result)
+    result = fetch_emails_from_today(config["MAIL_ADDRESS"], config["MAIL_PASSWORD"], config["MAIL_SERVER"], file_path='emails_today.txt')
+    print(result)
 
